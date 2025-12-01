f27127b927e9de50fd3a291395337ee4fff63940
rashhhe15 <rashmikaelangovan@gmail.com>
2025-12-01 18:45:47 +0530

    MySQL project files upload

---
+++ schema.sql
@@ -0,0 +1,21 @@
+USE carbon_calc;
+SELECT * FROM Transportation;
+SELECT * FROM Energy;
+SELECT * FROM Food;
+SET @user_id = 1;
+SET @distance_km = 15;
+SET @energy_units = 6;
+SET @transport_mode = 'Car';
+SET @food_type = (SELECT food_type FROM Food WHERE emission = 1.2 LIMIT 1);
+
+INSERT INTO CarbonEmissions (user_id, transport_emission, energy_emission, food_emission, total_emission)
+VALUES (
+  @user_id,
+  (SELECT emission_per_km FROM Transportation WHERE mode=@transport_mode LIMIT 1) * @distance_km,
+  (SELECT emission_per_unit FROM Energy WHERE source='Electricity' LIMIT 1) * @energy_units,
+  (SELECT emission FROM Food WHERE food_type=@food_type LIMIT 1),
+  ((SELECT emission_per_km FROM Transportation WHERE mode=@transport_mode LIMIT 1) * @distance_km) +
+  ((SELECT emission_per_unit FROM Energy WHERE source='Electricity' LIMIT 1) * @energy_units) +
+  ((SELECT emission FROM Food WHERE food_type=@food_id LIMIT 1))
+);
+SELECT * FROM CarbonEmissions;
